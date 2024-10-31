64f759d9dbb2aa929dff5b341e052714029d8fcd
Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>
2024-10-31 13:53:31 +0100

    bug colores arreglado
    Co-Authored-By: Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>

--- sdl2_simple_example/sdl2_simple_example/BasicForms.cpp
+++ sdl2_simple_example/sdl2_simple_example/BasicForms.cpp
@@ -39,33 +39,21 @@ void BasicForms::draw_Cube(const vec3& center, double size) {
 
 	glBegin(GL_TRIANGLES);  // Dibujar el cubo con triángulos
 
-	// Cara frontal (v0, v1, v2, v3) - Color: Verde
-	glColor4ub(0, 255, 0, 255); // Verde
 	glVertex3fv(v0); glVertex3fv(v1); glVertex3fv(v2);  // Primer triángulo
 	glVertex3fv(v2); glVertex3fv(v3); glVertex3fv(v0);  // Segundo triángulo
 
-	// Cara derecha (v1, v5, v6, v2) - Color: Azul
-	glColor4ub(0, 0, 255, 255); // Azul
 	glVertex3fv(v1); glVertex3fv(v5); glVertex3fv(v6);  // Primer triángulo
 	glVertex3fv(v6); glVertex3fv(v2); glVertex3fv(v1);  // Segundo triángulo
 
-    // Cara trasera (v5, v4, v7, v6) - Color: Rojo
-    glColor4ub(255, 0, 0, 255); // Rojo
     glVertex3fv(v5); glVertex3fv(v4); glVertex3fv(v7);  // Primer triángulo
     glVertex3fv(v7); glVertex3fv(v6); glVertex3fv(v5);  // Segundo triángulo
 
-    // Cara izquierda (v4, v0, v3, v7) - Color: Amarillo
-    glColor4ub(255, 255, 0, 255); // Amarillo
     glVertex3fv(v4); glVertex3fv(v0); glVertex3fv(v3);  // Primer triángulo
     glVertex3fv(v3); glVertex3fv(v7); glVertex3fv(v4);  // Segundo triángulo
 
-    // Cara superior (v3, v2, v6, v7) - Color: Naranja
-    glColor4ub(255, 165, 0, 255); // Naranja
     glVertex3fv(v3); glVertex3fv(v2); glVertex3fv(v6);  // Primer triángulo
     glVertex3fv(v6); glVertex3fv(v7); glVertex3fv(v3);  // Segundo triángulo
 
-    // Cara inferior (v4, v5, v1, v0) - Color: Cian
-    glColor4ub(0, 255, 255, 255); // Cian
     glVertex3fv(v4); glVertex3fv(v5); glVertex3fv(v1);  // Primer triángulo
     glVertex3fv(v1); glVertex3fv(v0); glVertex3fv(v4);  // Segundo triángulo
 
@@ -81,20 +69,11 @@ void BasicForms::draw_Pyramid(const vec3& center, double size) {
     static const GLfloat top[3] = { 0.0f,  1.0f,  0.0f }; // vértice superior
 
     glBegin(GL_TRIANGLES);  // Dibujar la pirámide con triángulos
-    // Cara frontal (v0, v1, top) - Color: Rojo
-    glColor4ub(255, 0, 0, 255);
+
     glVertex3fv(v0); glVertex3fv(v1); glVertex3fv(top);
-    // Cara derecha (v1, v2, top) - Color: Verde
-    glColor4ub(0, 255, 0, 255);
     glVertex3fv(v1); glVertex3fv(v2); glVertex3fv(top);
-    // Cara trasera (v2, v3, top) - Color: Azul
-    glColor4ub(0, 0, 255, 255);
     glVertex3fv(v2); glVertex3fv(v3); glVertex3fv(top);
-    // Cara izquierda (v3, v0, top) - Color: Amarillo
-    glColor4ub(255, 255, 0, 255);
     glVertex3fv(v3); glVertex3fv(v0); glVertex3fv(top);
-    // Base cuadrada (v0, v1, v2, v3) - Color: Cian
-    glColor4ub(0, 255, 255, 255);
     glVertex3fv(v0); glVertex3fv(v1); glVertex3fv(v2); // Primer triángulo
     glVertex3fv(v2); glVertex3fv(v3); glVertex3fv(v0); // Segundo triángulo
     glEnd();
