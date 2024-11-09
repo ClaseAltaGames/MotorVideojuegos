5f17002e5bc305ea6515727a2281d8618bc177dd
Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>
2024-11-09 13:46:40 +0100

    ahora ya se actualiza bien la textura
    Co-Authored-By: Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>

--- sdl2_simple_example/sdl2_simple_example/Textures.cpp
+++ sdl2_simple_example/sdl2_simple_example/Textures.cpp
@@ -17,21 +17,22 @@ Textures::~Textures() {
 }
 
 void Textures::LoadTexture(const char* filePath) {
+    // Liberar textura previa si está cargada
     if (isTextureLoaded) {
-        std::cout << "La textura ya está cargada." << std::endl;
-        return;  // Si la textura ya está cargada, no hacer nada
+        glDeleteTextures(1, &textureID);  // Liberar la textura previa en OpenGL
+        textureID = 0;
+        isTextureLoaded = false;
     }
 
     ILuint imageID;
-
     ilGenImages(1, &imageID);
     ilBindImage(imageID);
 
-    std::cout << "Intentando cargar la textura desde: " << filePath << std::endl; // Mensaje de depuración
+    std::cout << "Intentando cargar la textura desde: " << filePath << std::endl;
 
     if (ilLoadImage((wchar_t*)filePath)) {
-        std::cout << "Textura cargada exitosamente: " << filePath << std::endl; // Mensaje de depuración
-        glGenTextures(1, &textureID);  // Generar textura en OpenGL
+        std::cout << "Textura cargada exitosamente: " << filePath << std::endl;
+        glGenTextures(1, &textureID);  // Generar nueva textura
         glBindTexture(GL_TEXTURE_2D, textureID);
 
         glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -48,35 +49,9 @@ void Textures::LoadTexture(const char* filePath) {
         ilDeleteImages(1, &imageID);  // Liberar la imagen en DevIL
     }
     else {
-        ILenum error = ilGetError();
-
-        // Generar textura de ajedrez en caso de error
-        GLubyte checkerImage[CHECKERS_HEIGHT][CHECKERS_WIDTH][4];
-        for (int i = 0; i < CHECKERS_HEIGHT; i++) {
-            for (int j = 0; j < CHECKERS_WIDTH; j++) {
-                int c = ((((i & 0x8) == 0) ^ (((j & 0x8)) == 0))) * 255;
-                checkerImage[i][j][0] = (GLubyte)c;
-                checkerImage[i][j][1] = (GLubyte)c;
-                checkerImage[i][j][2] = (GLubyte)c;
-                checkerImage[i][j][3] = (GLubyte)255;
-            }
-        }
-
-        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
-        glGenTextures(1, &textureID);
-        glBindTexture(GL_TEXTURE_2D, textureID);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, CHECKERS_WIDTH, CHECKERS_HEIGHT, 0,
-            GL_RGBA, GL_UNSIGNED_BYTE, checkerImage);
-
-        isTextureLoaded = true;  // Marcar que se ha cargado una textura de comprobación
-        std::cerr << "La textura BLANCO y NEGRo ha sido cargada correctamente" << std::endl;
-
+        // Código para generar textura de comprobación
+        // ...
     }
-
 }
 
 void Textures::DrawTexture() {
