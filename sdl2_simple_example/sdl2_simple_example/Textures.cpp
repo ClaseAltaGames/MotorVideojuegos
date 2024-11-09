fa22b4f99ca4cd6052f8db8449d147743cc89550
Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>
2024-11-09 13:49:58 +0100

    checker texture
    Co-Authored-By: Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>

--- sdl2_simple_example/sdl2_simple_example/Textures.cpp
+++ sdl2_simple_example/sdl2_simple_example/Textures.cpp
@@ -1,4 +1,4 @@
-#include "Textures.h"
+ï»¿#include "Textures.h"
 #include <iostream>
 #include <string>
 #include "Component.h"
@@ -17,7 +17,7 @@ Textures::~Textures() {
 }
 
 void Textures::LoadTexture(const char* filePath) {
-    // Liberar textura previa si está cargada
+    // Liberar textura previa si estÃ¡ cargada
     if (isTextureLoaded) {
         glDeleteTextures(1, &textureID);  // Liberar la textura previa en OpenGL
         textureID = 0;
@@ -49,8 +49,33 @@ void Textures::LoadTexture(const char* filePath) {
         ilDeleteImages(1, &imageID);  // Liberar la imagen en DevIL
     }
     else {
-        // Código para generar textura de comprobación
-        // ...
+        ILenum error = ilGetError();
+
+        // Generar textura de ajedrez en caso de error
+        GLubyte checkerImage[CHECKERS_HEIGHT][CHECKERS_WIDTH][4];
+        for (int i = 0; i < CHECKERS_HEIGHT; i++) {
+            for (int j = 0; j < CHECKERS_WIDTH; j++) {
+                int c = ((((i & 0x8) == 0) ^ (((j & 0x8)) == 0))) * 255;
+                checkerImage[i][j][0] = (GLubyte)c;
+                checkerImage[i][j][1] = (GLubyte)c;
+                checkerImage[i][j][2] = (GLubyte)c;
+                checkerImage[i][j][3] = (GLubyte)255;
+            }
+        }
+
+        glPixelStorei(GL_UNPACK_ALIGNMENT, 1);
+        glGenTextures(1, &textureID);
+        glBindTexture(GL_TEXTURE_2D, textureID);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
+        glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, CHECKERS_WIDTH, CHECKERS_HEIGHT, 0,
+            GL_RGBA, GL_UNSIGNED_BYTE, checkerImage);
+
+        isTextureLoaded = true;  // Marcar que se ha cargado una textura de comprobaciï¿½n
+        std::cerr << "La textura BLANCO y NEGRO ha sido cargada correctamente" << std::endl;
+
     }
 }
 
