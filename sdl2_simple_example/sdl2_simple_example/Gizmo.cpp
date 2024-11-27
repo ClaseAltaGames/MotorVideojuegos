98d73c20e212f0c93015c088282e1cf501922060
Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>
2024-11-27 12:05:12 +0100

    gizmo se ve perfecto
    Co-Authored-By: Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>

--- sdl2_simple_example/sdl2_simple_example/Gizmo.cpp
+++ sdl2_simple_example/sdl2_simple_example/Gizmo.cpp
@@ -9,18 +9,32 @@ Gizmo::~Gizmo()
 }
 
 void Gizmo::drawTranslateGizmo() {
-
     glDisable(GL_DEPTH_TEST);
-    glColor3f(1.0f, 0.0f, 0.0f); // Rojo para X
-    basicForms->drawArrow(glm::vec3(1.0f, 0.0f, 0.0f));
 
-    glColor3f(0.0f, 1.0f, 0.0f); // Verde para Y
-    basicForms->drawArrow(glm::vec3(0.0f, 1.0f, 0.0f));
+    // Dibujar flecha en X (Rojo)
+    glPushMatrix();
+    glColor3f(1.0f, 0.0f, 0.0f);
+    glTranslatef(1.0f, 0.0f, 0.0f);  // Trasladar en X
+    glRotatef(90.0f, 0.0f, 1.0f, 0.0f); // Rotar 90° sobre Y
+    basicForms->drawArrow(glm::vec3(0.0f, 0.0f, 0.0f));
+    glPopMatrix();
 
-    glColor3f(0.0f, 0.0f, 1.0f); // Azul para Z
-    basicForms->drawArrow(glm::vec3(0.0f, 0.0f, 1.0f));
+    // Dibujar flecha en Y (Verde)
+    glPushMatrix();
+    glColor3f(0.0f, 1.0f, 0.0f);
+    glTranslatef(0.0f, 1.0f, 0.0f);  // Trasladar en Y
+    glRotatef(-90.0f, 1.0f, 0.0f, 0.0f); // Rotar -90° sobre X
+    basicForms->drawArrow(glm::vec3(0.0f, 0.0f, 0.0f));
+    glPopMatrix();
+
+    // Dibujar flecha en Z (Azul)
+    glPushMatrix();
+    glColor3f(0.0f, 0.0f, 1.0f);
+    glTranslatef(0.0f, 0.0f, 1.0f);  // Trasladar en Z
+    basicForms->drawArrow(glm::vec3(0.0f, 0.0f, 0.0f));
+    glPopMatrix();
+
     glEnable(GL_DEPTH_TEST);
-
 }
 void Gizmo::drawScaleGizmo() {
     glDisable(GL_DEPTH_TEST);
